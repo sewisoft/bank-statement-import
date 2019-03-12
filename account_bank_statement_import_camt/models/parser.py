a8e2f1344fdf7fce313b8971df6df67116f5f2a6
Günter Selbert <guenter.selbert@sewisoft.de>
2019-03-12 19:45:03 +0100

    [IMP] better reference and use date of transaction

--- account_bank_statement_import_camt/models/parser.py
+++ account_bank_statement_import_camt/models/parser.py
@@ -65,6 +65,7 @@ class CamtParser(models.AbstractModel):
             ns, node, [
                 './ns:RmtInf/ns:Strd/ns:CdtrRefInf/ns:Ref',
                 './ns:Refs/ns:EndToEndId',
+                './ns:Ntry/ns:AcctSvcrRef'
             ],
             transaction, 'ref'
         )
@@ -135,10 +136,10 @@ class CamtParser(models.AbstractModel):
             ns, node, [
                 './ns:NtryDtls/ns:RmtInf/ns:Strd/ns:CdtrRefInf/ns:Ref',
                 './ns:NtryDtls/ns:Btch/ns:PmtInfId',
+                './ns:NtryDtls/ns:TxDtls/ns:Refs/ns:AcctSvcrRef'
             ],
             transaction, 'ref'
         )
-
         details_nodes = node.xpath(
             './ns:NtryDtls/ns:TxDtls', namespaces={'ns': ns})
         if len(details_nodes) == 0:
@@ -205,6 +206,12 @@ class CamtParser(models.AbstractModel):
         for entry_node in entry_nodes:
             transactions.extend(self.parse_entry(ns, entry_node))
         result['transactions'] = transactions
+        result['date'] = None
+        if transactions:
+            result['date'] = sorted(transactions,
+                                    key=lambda x: x['date'],
+                                    reverse=True
+                                    )[0]['date']
         return result
 
     def check_version(self, ns, root):
